927f2fbb776c83aa258b66ca35bf051a5d251419
Oleg <okovalchuk722@gmail.com>
2024-10-16 13:42:53 +0300

    script8.12

---
+++ script_8.12.js
@@ -0,0 +1,7 @@
+/*In this simple exercise, you will create a program that will take two lists of integers, a and b.
+Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b.
+You must find the difference of the cuboids' volumes regardless of which is bigger.*/
+
+function findDifference(a, b) {
+	return Math.abs((a[0] * a[1] * a[2]) - (b[0] * b[1] * b[2]));
+}
