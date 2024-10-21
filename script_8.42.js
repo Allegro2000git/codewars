c8a8bb641fd1bf89345c87735f12cf8983d40b8a
Oleg <okovalchuk722@gmail.com>
2024-10-21 12:05:00 +0300

    script8.42

--- script_8.42.js
+++ script_8.42.js
@@ -2,3 +2,8 @@
 
 You can assume, for the purpose of this kata, that the supplied array will not be empty. */
 
+function findSmallestInt(arr) {
+
+	let sorted = arr.sort((a, b) => a - b)
+	return sorted[0]
+}
