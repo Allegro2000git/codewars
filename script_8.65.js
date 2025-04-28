e354145b72250bfc10df290733507aa3b4c64871
Oleg Kova <okovalchuk722@gmail.com>
2025-04-28 13:03:23 +0300

    script8.65

---
+++ script_8.65.js
@@ -0,0 +1,6 @@
+//Write a function to split a string and convert it into an array of words.
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+function stringToArray(string){
+    return string.split(' ')
+}
