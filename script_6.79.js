962e65ef01a376c14b908e15fb6d25c2cf439c78
Oleg Kova <okovalchuk722@gmail.com>
2025-05-20 13:14:19 +0300

    script6.79

---
+++ script_6.79.js
@@ -0,0 +1,11 @@
+//The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+//
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+    let res = string.split('').reduce((acc, el) => {
+        acc[el] = (acc[el] || 0) + 1
+        return acc
+    }, {})
+    return res
+}
