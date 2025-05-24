a8e9f2f53b53ca1ab67b7831f4d88468f2a7f789
Oleg Kova <okovalchuk722@gmail.com>
2025-05-24 11:24:57 +0300

    script8.81

---
+++ script_8.81.js
@@ -0,0 +1,20 @@
+/*Create a function that accepts a string and a single character, and returns an integer of
+the count of occurrences the 2nd argument is found in the first one.
+
+If no occurrences can be found, a count of 0 should be returned.
+
+("Hello", 'o')  =>  1
+("Hello", 'l')  =>  2
+("", 'z')       =>  0
+Notes
+The first argument can be an empty string
+*/
+
+function strCount(str, letter){
+    return res = str.split('').reduce((acc, i) => {
+        if (i === letter) {
+            acc += 1
+        }
+        return acc
+    }, 0 )
+}
