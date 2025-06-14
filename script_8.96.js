2bc30e12a410a4b9ed4e82ea4b2c05237e18e29d
Oleg Kova <okovalchuk722@gmail.com>
2025-06-14 15:31:42 +0300

    script8.96

---
+++ script_8.96.js
@@ -0,0 +1,14 @@
+/*
+* Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+*/
+
+function getChar(c){
+    const char = String.fromCharCode(c)
+    return  char
+}
