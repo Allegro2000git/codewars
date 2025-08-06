711a80c814002149ecabc6ce26b7bef81e37f620
Oleg Kova <okovalchuk722@gmail.com>
2025-08-06 15:31:06 +0300

    script6.8

---
+++ script_6.8.js
@@ -0,0 +1,30 @@
+/*
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+*/
+
+function sortArray(array) {
+    const nechet = []
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 !== 0) {
+            nechet.push(array[i])
+            array[i] = "x"
+        }
+    }
+    const sorted = nechet.sort((a,b) => a - b)
+
+    let nechetIndex = 0;
+
+    for (let y = 0; y < array.length; y++) {
+        if (array[y] === "x") {
+            array[y] = nechet[nechetIndex]
+            nechetIndex++
+        }
+    }
+    return array
+}
