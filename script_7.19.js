7abeb023a629bbbb37dc759af2869a095284ac67
Oleg Kova <okovalchuk722@gmail.com>
2025-08-09 11:49:58 +0300

    script7.19

--- script_7.19.js
+++ script_7.19.js
@@ -11,25 +11,8 @@ mxdiflg(a1, a2) --> 13
 */
 
 function mxdiflg(a1, a2) {
-
-    let result= 0
-
-    if (a1.length === 0 || a2.length === 0 ) {
-        return -1
-    }
-
-    for (let i = 0; i < a1.length; i++) {
-        for (let j = 0; j < a2.length; j++) {
-            let length1 = a1[i].length
-            let length2 = a2[j].length
-
-            if (result < length1 - length2) {
-                result = length1 - length2
-            } else if (result < length2 - length1){
-                result = length2 - length1
-            }
-        }
-    }
-    return result
-
+    if (a1.length === 0 || a2.length === 0) return -1
+    let l1 = a1.map(str => str.length)
+    let l2 = a2.map(str => str.length)
+    return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
 }
