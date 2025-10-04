569e7b5cb2327185948573f0e81a2526570b2df0
Oleg Kova <okovalchuk722@gmail.com>
2025-10-04 14:43:23 +0300

    script7.2

---
+++ script_7.2.js
@@ -0,0 +1,15 @@
+/*
+Your task is to return a number from a string.
+Details
+You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+*/
+
+function filterString(value) {
+    let str = ''
+    for (let el of value) {
+        if (Number.parseInt(el, 10) ||Number.parseInt(el, 10) === 0 ) {
+            str += el
+        }
+    }
+    return +str
+}
